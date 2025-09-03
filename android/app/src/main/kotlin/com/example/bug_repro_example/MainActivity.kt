d7f0b55014feba0ab39990a73d23ab57d18aac25
PlayCulture <info@playculture.com>
2025-09-03 15:38:06 +0100

    Initial commit: Minimum example

---
+++ android/app/src/main/kotlin/com/example/bug_repro_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bug_repro_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  bool firstScannerActive = false;
+  bool secondScannerActive = false;
+
+  void toggleSecondScanner() async {
+    if (secondScannerActive) {
+      setState(() {firstScannerActive = false; secondScannerActive = false;});
+      await Future.delayed(Duration(milliseconds: 250));
+      setState(() {firstScannerActive = true;});
+    } else if (!secondScannerActive) {
+      setState(() {secondScannerActive = true;});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(appBar: 
+    
+    AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title),), body: 
+      Center(child: 
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            
+          const Text('Tap the button to toggle loading the second instance of MobileScanner', textAlign: TextAlign.center),
+
+          firstScannerActive ?
+          SizedBox(width: 200.0, height: 200.0, child:
+          MobileScanner(
+            onDetect: (capture) {
+              final List<Barcode> barcodes = capture.barcodes;
+              for (final barcode in barcodes) {
+                debugPrint('Barcode found! ${barcode.rawValue}');
+              }
+            },
+            onDetectError: (context, error) {
+              print("Error: "+error.toString());
+            },
+          ),
+          ) : const SizedBox(height: 0.0) ,
+
+          secondScannerActive ?
+          SizedBox(width: 200.0, height: 200.0, child:
+          MobileScanner(
+            onDetect: (capture) {
+              final List<Barcode> barcodes = capture.barcodes;
+              for (final barcode in barcodes) {
+                debugPrint('Barcode found! ${barcode.rawValue}');
+              }
+            },
+            onDetectError: (context, error) {
+              print("Error: "+error.toString());
+            },
+          ),
+          ) : const SizedBox(height: 0.0) ,
+            
+        ],),
+      ),
+      floatingActionButton: 
+      FloatingActionButton(onPressed: toggleSecondScanner, tooltip: 'Toggle Second Scanner', child: 
+        const Icon(Icons.add),
+      ),
+    );
+  }
+}
